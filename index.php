ba910a2af3ee59ec2809355ce7a823c9f9a08594
Martin Meredith <martin@sourceguru.net>
2014-06-01 22:49:09 +0100

    Add player list

--- index.php
+++ index.php
@@ -19,22 +19,39 @@ $user = new User();
 $user->setAdapter($adapter);
 
 $mez_id = $user->resolveVanityUrl('mezzle')['response']['steamid'];
+
+$user = new User();
+$user->setAdapter($adapter);
 $tetcher_id = $user->resolveVanityUrl('tetcher')['response']['steamid'];
+
+$user = new User();
+$user->setAdapter($adapter);
 $emek_id = $user->resolveVanityUrl('emekcrash')['response']['steamid'];
 
 $other_id = '76561198028082641';
 
 $players = [$tetcher_id, $emek_id, $other_id];
 
-$playerService = new PlayerService();
-$playerService->setAdapter($adapter);
-
 $games = [];
 
+$player_games = [];
+
+$player_nicks = [
+    $mez_id => 'Mez',
+    $tetcher_id => 'Tetcher',
+    $emek_id => 'Eman',
+    $other_id => 'Ethan'
+];
+
 foreach ($players as $player) {
+    $playerService = new PlayerService();
+    $playerService->setAdapter($adapter);
     $result = $playerService->getOwnedGames($mez_id, true);
     foreach ($result['response']['games'] as $game) {
+
         $game_id = $game['appid'];
+
+        $player_games[$game_id][] = $player_nicks[$player];
         $games[$game_id] = $game;
     }
 }
@@ -52,6 +69,16 @@ $random_game = array_shift($games);
 <div style="margin: 0 auto;">
 <h2><?php echo $random_game['name']; ?></h2>
 <img src="http://media.steampowered.com/steamcommunity/public/images/apps/<?php echo $random_game['appid']; ?>/<?php echo $random_game['img_logo_url']; ?>.jpg" />
+
+<ul>
+<?php
+$appid = $random_game['appid'];
+
+foreach ($player_games[$appid] as $player) {
+    ?><li><?php echo $player; ?></li><?php
+}
+?>
+</ul>
 </div>
 </body>
 </html>
