ad51d5c94dac3a939f0d32e394cf981df32fbda8
Martin Meredith <martin@sourceguru.net>
2014-06-01 22:08:59 +0100

    Testing vanity URL resolution;

--- index.php
+++ index.php
@@ -1,3 +1,25 @@
 <?php
 
 require_once 'vendor/autoload.php';
+
+use JMS\Serializer\SerializerBuilder;
+use Steam\Adapter\Guzzle;
+use Steam\Configuration;
+use Steam\Api\User;
+
+$config = new Configuration(array(
+    'steamKey' => 'B05AC3BD9B29681761D2DA83263F2D1E',
+));
+
+$adapter = new Guzzle($config);
+$adapter->setSerializer(SerializerBuilder::create()->build());
+
+$user = new User();
+$user->setAdapter($adapter);
+
+var_dump($user->resolveVanityUrl('mezzle'));
+
+
+
+
+var_dump($user->resolveVanityUrl('tetcher'));
