272bed0f5d26a81ee7c0adb472368cc369e0101b
Martin Meredith <martinmeredith@protecinnovations.co.uk>
2014-06-02 10:40:29 +0100

    Error pages

--- config/application.config.php
+++ config/application.config.php
@@ -17,18 +17,5 @@ return [
     'view_manager' => [
         'display_not_found_reason' => true,
         'display_exceptions' => true,
-        'doctype' => 'HTML5',
-        'not_found_template' => 'error/404',
-        'exception_template' => 'error/index',
-        'template_map' => [
-            'layout/ajax' => realpath(__DIR__ . '/../view/layout/ajax.twig'),
-            'layout/layout' => realpath(__DIR__ . '/../view/layout/layout.twig'),
-            'layout/login' => realpath(__DIR__ . '/../view/layout/noui.twig'),
-            'error/404' => realpath(__DIR__ . '/../view/error/404.twig'),
-            'error/index' => realpath(__DIR__ . '/../view/error/index.twig'),
-        ],
-        'template_path_stack' => [
-            realpath(__DIR__ . '/../view'),
-        ],
     ],
 ];
--- module/Steam/config/config.php
+++ module/Steam/config/config.php
@@ -6,4 +6,19 @@ return [
             'roulette' => '\GamerPowered\Roulette\RouletteController'
         ]
     ],
+    'view_manager' => [
+        'doctype' => 'HTML5',
+        'not_found_template' => 'error/404',
+        'exception_template' => 'error/index',
+        'template_map' => [
+/*            'layout/ajax' => realpath(__DIR__ . '/../view/layout/ajax.twig'),
+            'layout/layout' => realpath(__DIR__ . '/../view/layout/layout.twig'),
+            'layout/login' => realpath(__DIR__ . '/../view/layout/noui.twig'), */
+            'error/404' => realpath(__DIR__ . '/../view/error/404.twig'),
+            'error/index' => realpath(__DIR__ . '/../view/error/index.twig'),
+        ],
+        'template_path_stack' => [
+            realpath(__DIR__ . '/../view'),
+        ],
+    ],
 ];
