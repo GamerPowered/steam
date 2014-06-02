bc2944e7303c90378140b2cf56ac9f36b9f92bdb
Martin Meredith <martinmeredith@protecinnovations.co.uk>
2014-06-02 10:13:44 +0100

    Minor changes

--- config/application.config.php
+++ config/application.config.php
@@ -2,6 +2,7 @@
 
 return [
     'modules' => [
+        'ZfcTwig',
         'GamerPowered\Steam',
     ],
 ];
--- module/Steam/config/config.php
+++ module/Steam/config/config.php
@@ -6,5 +6,4 @@ return [
             'roulette' => '\GamerPowered\Roulette\RouletteController'
         ]
     ],
-    ''
 ];
