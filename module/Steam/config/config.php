bb868770050642b77142257319917ad2dc594600
Martin Meredith <martinmeredith@protecinnovations.co.uk>
2014-06-02 10:44:02 +0100

    Error pages

--- module/Steam/config/config.php
+++ module/Steam/config/config.php
@@ -10,13 +10,13 @@ return [
         'doctype' => 'HTML5',
         'not_found_template' => 'error/404',
         'exception_template' => 'error/index',
-        'template_map' => [
-/*            'layout/ajax' => realpath(__DIR__ . '/../view/layout/ajax.twig'),
+/*        'template_map' => [
+            'layout/ajax' => realpath(__DIR__ . '/../view/layout/ajax.twig'),
             'layout/layout' => realpath(__DIR__ . '/../view/layout/layout.twig'),
-            'layout/login' => realpath(__DIR__ . '/../view/layout/noui.twig'), */
+            'layout/login' => realpath(__DIR__ . '/../view/layout/noui.twig'),
             'error/404' => realpath(__DIR__ . '/../view/error/404.twig'),
             'error/index' => realpath(__DIR__ . '/../view/error/index.twig'),
-        ],
+        ], */
         'template_path_stack' => [
             realpath(__DIR__ . '/../view'),
         ],
