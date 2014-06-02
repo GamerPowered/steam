962662a9561b5d1b560f8db2c4ad772c6e8ba2f4
Martin Meredith <martinmeredith@protecinnovations.co.uk>
2014-06-02 14:29:13 +0100

    Enter stuff

--- module/Steam/src/Roulette/RouletteController.php
+++ module/Steam/src/Roulette/RouletteController.php
@@ -33,7 +33,7 @@ class RouletteController extends AbstractActionController
         $user = $this->getServiceLocator()->get('\GamerPowered\Steam\Api\User');
 
         foreach ($to_resolve as $resolvee) {
-            $player_id = $user->resolveVanityUrl($to_resolve);
+            $player_id = $user->resolveVanityUrl($resolvee);
 
             if (!is_null($player_id)) {
                 $players[] = $player_id;
