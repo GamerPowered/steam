0acf282e4a5d3c7c25f6ccc3b717ea68faebeefa
Martin Meredith <martin@sourceguru.net>
2014-06-06 20:46:23 +0100

    Add json/show.twig?

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -6,7 +6,7 @@ return [
     ],
     'view_manager' => [
         'display_not_found_reason' => false,
-        'display_exceptions' => true,
+        'display_exceptions' => getenv('DISPLAY_EXCEPTIONS'),
     ],
     'zfctwig' => [
         'disable_zf_model' => false,
