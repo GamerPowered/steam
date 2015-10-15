8f7473ef423fb94fcdce9908a2be38ef052f30c9
Martin Meredith <martinmeredith@protecinnovations.co.uk>
2015-10-15 16:27:40 +0100

    Airbrake

--- public/index.php
+++ public/index.php
@@ -1,6 +1,7 @@
 <?php
 
-use \Zend\Mvc\Application;
+use Airbrake;
+use Zend\Mvc\Application;
 
 date_default_timezone_set('Europe/London');
 
@@ -13,5 +14,18 @@ if (!include_once('vendor/autoload.php')) {
 
 // Get application stack configuration
 $config = include 'config/application.config.php';
+
+// Create new Notifier instance.
+$notifier = new Airbrake\Notifier(array(
+    'projectId' => 116732,
+    'projectKey' => '20c3949e27b50d1921dcd377a5f1851a',
+));
+
+// Set global notifier instance.
+Airbrake\Instance::set($notifier);
+
+// Register error and exception handlers.
+$handler = new Airbrake\ErrorHandler($notifier);
+$handler->register();
 
 Application::init($config)->run();
